83aac59118d81fb29230a559ee328165e700e0f9
GalacticKatAlpha <524721@chrome.escg.ac.uk>
2023-03-31 16:16:43 +0100

    wavespawner

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    private WaveSpawner waveSpawner;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start ()
+    {
+        waveSpawner = GetComponentInParent<WaveSpawner>();
+        target = Waypoints.waypoints[0];
+    }
+
+    void Update ()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+
+            waveSpawner.waves[waveSpawner.currentWaveIndex].enemiesLeft--;
+
+            
+        }
+
+        wavepointIndex++;
+        target = Waypoints.waypoints[wavepointIndex];
+    }
+}
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,28 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
     [SerializeField] private float countdown;
 
+    [SerializeField] private GameObject spawnPoint;
+
     public Wave[] waves;
 
-    private int currentWaveIndex = 0;
+    public int currentWaveIndex = 0;
 
-    private void Update()
+    private bool readyToCountDown;
+
+    private void start()
     {
-        countdown -= Time.deltaTime;
+        readyToCountDown = true;
 
-        if (countdown <= 0)
+        for (int i = 0; i < waves.Length; i++)
         {
-            SpawnWave();
+            waves[i].enemiesLeft = waves[i].enemies.Length;
         }
     }
 
-    private void SpawnWave()
+    private void Update()
     {
-        
+        if (currentWaveIndex >= waves.Length)
+        {
+            Debug.Log("TEST");
+            return;
+        }
+
+        if(readyToCountDown == true)
+        {
+            countdown -= Time.deltaTime;
+        }
+
+        if (countdown <= 0)
+        {
+            readyToCountDown = false;
+
+            countdown = waves[currentWaveIndex].timeToNextWave;
+
+            StartCoroutine(SpawnWave());
+        }
+
+        if (waves[currentWaveIndex].enemiesLeft == 0)
+        {
+            readyToCountDown = true;
+            currentWaveIndex++;
+        }
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        if (currentWaveIndex < waves.Length)
+        {
+            for (int i = 0; i < waves[currentWaveIndex].enemies.Length; i++)
+            {
+                Enemy enemy = Instantiate(waves[currentWaveIndex].enemies[i],spawnPoint.transform);
+
+                enemy.transform.SetParent(transform);
+
+                yield return new WaitForSeconds(waves[currentWaveIndex].timeToNextEnemy);
+            }
+        }
     }
 
 }
@@ -31,6 +75,10 @@ public class WaveSpawner : MonoBehaviour
 public class Wave
 {
     public Enemy[] enemies;
+    public float timeToNextEnemy;
+    public float timeToNextWave;
+
+    [HideInInspector] public int enemiesLeft;
 }
 
 
